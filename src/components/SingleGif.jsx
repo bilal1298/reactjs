2bf3816a8157e8280a3d4b571d2320b609bdcce2
bilal1298 <58294614+bilal1298@users.noreply.github.com>
2022-11-10 01:35:56 +0530

    Update SingleGif.jsx

--- src/components/SingleGif.jsx
+++ src/components/SingleGif.jsx
@@ -3,18 +3,19 @@ import { useContext } from "react";
 import { useParams } from "react-router-dom";
 import MyContext from "../MyContext";
 import "../App.css";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const SingleGif = () => {
+  const navigate = useNavigate();
   const { name } = useParams();
   const { state } = useContext(MyContext);
   const image = state.data.find((item) => item.id === name);
   console.log(image);
   return (
     <div className="singleContainer">
-      <Link to="/" className="goBack">
+      <button onClick={() => navigate(-1)} className="goBack">
         &lt; Go Back
-      </Link>
+      </button>
       <img src={image.images.original.url} alt="" />
     </div>
   );
